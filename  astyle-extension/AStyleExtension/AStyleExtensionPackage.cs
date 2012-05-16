2e36628bba736eb3beb23f5b276bb767ccfe97ec
lukamicoder <lukamicoder@9cc1628e-0bdd-c411-540f-70e47e6e954c>
2012-05-16 04:16:24 +0000

    Menu commands are only enabled when the active document is a C/C++ or C# document. Upgraded to version 1.2.
    git-svn-id: https://astyle-extension.googlecode.com/svn/trunk@13 9cc1628e-0bdd-c411-540f-70e47e6e954c

---  astyle-extension/AStyleExtension/AStyleExtensionPackage.cs
+++  astyle-extension/AStyleExtension/AStyleExtensionPackage.cs
@@ -9,16 +9,16 @@ using Microsoft.VisualStudio.Shell;
 
 namespace AStyleExtension {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [InstalledProductRegistration("#110", "#112", "1.2", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideOptionPage(typeof(AStyleGeneralOptionsPage), "AStyle Formatter", "General", 0, 0, true)]
+    [ProvideAutoLoad("{f1536ef8-92ec-443c-9ed7-fdadf150da82}")]
     [Guid(GuidList.GuidPkgString)]
     public sealed class AStyleExtensionPackage : Package {
-        private static IVsOutputWindowPane _outputPane;
         private DTE _dte;
         private Document _doc;
-        private MenuCommand _formatSelMenuCommand;
-        private MenuCommand _formatDocMenuCommand;
+        private OleMenuCommand _formatSelMenuCommand;
+        private OleMenuCommand _formatDocMenuCommand;
         private bool _isCSharpEnabled;
         private Properties _props;
 
@@ -28,43 +28,62 @@ namespace AStyleExtension {
             OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if (null != mcs) {
                 CommandID id = new CommandID(GuidList.GuidCmdSet, (int)PkgCmdIDList.FormatDocumentCommand);
-                _formatDocMenuCommand = new MenuCommand(FormatDocumentCallback, id);
+                _formatDocMenuCommand = new OleMenuCommand(FormatDocumentCallback, id);
                 mcs.AddCommand(_formatDocMenuCommand);
+                _formatDocMenuCommand.BeforeQueryStatus += OnBeforeQueryStatus;
 
                 id = new CommandID(GuidList.GuidCmdSet, (int)PkgCmdIDList.FormatSelectionCommand);
-                _formatSelMenuCommand = new MenuCommand(FormatSelectionCallback, id);
+                _formatSelMenuCommand = new OleMenuCommand(FormatSelectionCallback, id);
                 mcs.AddCommand(_formatSelMenuCommand);
-            }
+                _formatSelMenuCommand.BeforeQueryStatus += OnBeforeQueryStatus;
 
-            var outputWindow = GetService(typeof(SVsOutputWindow)) as IVsOutputWindow;
-            if (outputWindow != null) {
-                Guid guidPane = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
-                outputWindow.GetPane(ref guidPane, out _outputPane);
             }
 
             _dte = (DTE)GetService(typeof(DTE));
 
             if (_dte.RegistryRoot.Contains("VisualStudio")) {
-               _isCSharpEnabled = true;
+                _isCSharpEnabled = true;
             }
 
             _props = _dte.Properties["AStyle Formatter", "General"];
             _props.Item("IsCSarpEnabled").Value = _isCSharpEnabled;
         }
 
-        private void FormatDocumentCallback(object sender, EventArgs e) {
+        private bool GetActiveDocument(out TextDocument textDoc, out string language) {
+            textDoc = null;
+            language = null;
+
             _doc = _dte.ActiveDocument;
             if (_doc == null) {
-                return;
-            }
-            
-            TextDocument textDoc = _doc.Object("TextDocument") as TextDocument;
-            if (textDoc == null) {
-                return;
+                return false;
             }
 
-            string language = textDoc.Language.ToLower();
-            if (language != "avrgcc" && language != "c/c++" && (language != "csharp" || !_isCSharpEnabled)) {
+            textDoc = _doc.Object("TextDocument") as TextDocument;
+            if (textDoc == null) {
+                return false;
+            }
+
+            language = textDoc.Language.ToLower();
+            if (language != "gcc" && language != "avrgcc" && language != "c/c++" && (language != "csharp" || !_isCSharpEnabled)) {
+                return false;
+            }
+
+            return true;
+        }
+
+        void OnBeforeQueryStatus(object sender, EventArgs e) {
+            OleMenuCommand cmd = (OleMenuCommand)sender;
+            TextDocument textDoc;
+            string language;
+            
+            cmd.Visible = GetActiveDocument(out textDoc, out language);
+        }
+
+        private void FormatDocumentCallback(object sender, EventArgs e) {
+            TextDocument textDoc;
+            string language;
+
+            if (!GetActiveDocument(out textDoc, out language)) {
                 return;
             }
 
@@ -81,18 +100,10 @@ namespace AStyleExtension {
         }
 
         private void FormatSelectionCallback(object sender, EventArgs e) {
-            _doc = _dte.ActiveDocument;
-            if (_doc == null) {
-                return;
-            }
+            TextDocument textDoc;
+            string language;
 
-            TextDocument textDoc = _doc.Object("TextDocument") as TextDocument;
-            if (textDoc == null) {
-                return;
-            }
-
-            string language = textDoc.Language.ToLower();
-            if (language != "c/c++" && (language != "csharp" || !_isCSharpEnabled)) {
+            if (!GetActiveDocument(out textDoc, out language)) {
                 return;
             }
 
@@ -106,7 +117,7 @@ namespace AStyleExtension {
         }
 
         string Format(string text, string language) {
-            string options; 
+            string options;
 
             if (_props == null) {
                 MessageBox.Show("Unable to read AStyle Formatter settings.", "AStyle Formatter Error");
@@ -115,7 +126,7 @@ namespace AStyleExtension {
 
             if (_isCSharpEnabled && language == "csharp") {
                 options = (string)_props.Item("CsOptions").Value;
-            } else if (language == "c/c++" || language == "avrgcc") {
+            } else if (language == "c/c++" || language == "avrgcc" || language == "gcc") {
                 options = (string)_props.Item("CppOptions").Value;
             } else {
                 return null;
