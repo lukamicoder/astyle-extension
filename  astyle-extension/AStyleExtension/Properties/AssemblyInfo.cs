bae740aca821990bfc9fb1f45a035e106227a223
lukamicoder <lukamicoder@9cc1628e-0bdd-c411-540f-70e47e6e954c>
2013-09-17 23:09:10 +0000

    Added support for Visual Studio 2013. Upgraded to version 2.1.
    git-svn-id: https://astyle-extension.googlecode.com/svn/trunk@30 9cc1628e-0bdd-c411-540f-70e47e6e954c

---  astyle-extension/AStyleExtension/Properties/AssemblyInfo.cs
+++  astyle-extension/AStyleExtension/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]     
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
 
 
 
